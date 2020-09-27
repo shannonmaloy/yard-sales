66da67947a4a1634eab7fc47dbf57dc3c04cbb50
Shannon Maloy <shannon.maloy@gmail.com>
2020-09-27 14:21:32 -0400

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,8 @@ Rails.application.routes.draw do
   get :logged_in, to: "sessions#logged_in"
   root to: "static#home"
 
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
+
 end
