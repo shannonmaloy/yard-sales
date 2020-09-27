018b58bdcbe5464885613b5439cbe0514f65d242
Shannon Maloy <shannon.maloy@gmail.com>
2020-09-27 01:58:07 -0400

    Search bar filters data

--- client/src/components/FindSales.js
+++ client/src/components/FindSales.js
@@ -6,92 +6,82 @@ import {LoadScript} from '@react-google-maps/api'
 
 export default class FindASale extends Component {
     constructor(props) {
-    super(props)
-    this.state = {
-        searchBarInput: "",
-        data: null,
-        filteredData: null,
-        geocodedSearchBarInput: null,
+        super(props)
+        this.state = {
+            searchBarInput: "",
+            data: null,
+            filteredData: null,
+            geocodedSearchBarInput: null,
 
-    }
+        }
         this.handleSubmit = this.handleSubmit.bind(this)
         this.handleChange = this.handleChange.bind(this)
-        this.geocodeSearchBarInput = this.geocodeSearchBarInput.bind(this)
+       
     }
 
-    
-    
     getData = () => {
         axios.get('http://localhost:3001/sales')
             .then(res => {
-                
                 this.setState({ data: (res.data.sales) }
                 )
             }).catch(err => {
                 console.log("check login", err)
-              })
-    } 
-    
-    handleChange(event) {
-        this.setState({
-        [event.target.name]: event.target.value
-        })
+            })
     }
     
+    
+    
     handleSubmit(event) {
+        event.preventDefault()
         this.setState({ [event.target.name]: event.target.value })
         this.geocodeSearchBarInput(this.state.searchBarInput)
-        this.filterData()
+    
         console.log("THIS from FindSale", this)
         console.log(this.state)
-        event.preventDefault()
+        
+    }
+
+    handleChange(event) {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
     }
     //Haversine Formula for calculating distance between two lat/lng coordinates
     distance = (lat1, lng1, lat2, lng2) => {
         const p = 0.017453292519943295    // Math.PI / 180 convert degrees to radians
         const c = Math.cos;
-        let a = 0.5 - c((lat2 - lat1) * p)/2 + 
-                c(lat1 * p) * c(lat2 * p) * 
-                (1 - c((lng2 - lng1) * p)) / 2
+        let a = 0.5 - c((lat2 - lat1) * p) / 2 +
+            c(lat1 * p) * c(lat2 * p) *
+            (1 - c((lng2 - lng1) * p)) / 2
       
         return 7917.6 * Math.asin(Math.sqrt(a)); // 7917.6 = 2 * R; R = 3958.8 radius of Earth in miles
-      }
+    }
 
     filterData = () => {
         let filteredDataArr = []
         this.state.data.map(place => {
-          console.log("Sale ID: ", place.id, "Lat/Lng: ", { lat: parseFloat(place.lat), lng: parseFloat(place.lng) }, "Geocode Result:", )
-          let distanceInMiles = this.distance(place.lat, place.lng, 33.860649, -84.339790)
-          console.log("Distance from center: ", distanceInMiles, this.state.searchBarInput)
-          if( distanceInMiles < 10) {
-              console.log("Inside 66")
-              filteredDataArr.push(place)
-          }
+            console.log("Sale ID: ", place.id, "Lat/Lng: ", { lat: parseFloat(place.lat), lng: parseFloat(place.lng) }, "Geocode Result:", this.state.geocodedSearchBarInput.lat)
+            let distanceInMiles = this.distance(place.lat, place.lng, this.state.geocodedSearchBarInput.lat, this.state.geocodedSearchBarInput.lng)
+            console.log("Distance from center: ", distanceInMiles, this.state.searchBarInput)
+            if (distanceInMiles < 10) {
+                console.log("Inside 66")
+                filteredDataArr.push(place)
+            }
         });
-        this.setState({filteredData: filteredDataArr})
-      };
+        this.setState({ filteredData: filteredDataArr })
+    };
     
     geocodeSearchBarInput = (searchBarInput) => {
-        return new Promise(function (resolve, reject) {
-            console.log("Hello", searchBarInput)
-            const geocoder = new google.maps.Geocoder()
-         
-            geocoder.geocode({ address: searchBarInput }, function (results, status) {
-                
-                if (status === "OK") {
-                    console.log("Hello geocoded address", status, "Then ", results[0].geometry.location.toJSON())
-                    
-                    resolve(this.setState({geocodedSearchBarInput: results[0].geometry.location.toJSON()}))
-                } else {
-                    reject(new Error("Could not locate " + searchBarInput))
-                }
-            }.bind(this))
-        }.bind(this))
+       
+        const geocoder = new google.maps.Geocoder()
+        geocoder.geocode({ address: searchBarInput }, (results, status) => {
+            console.log("Hello geocoded address", status, "Then ", results[0].geometry.location.toJSON())
+            this.setState({ geocodedSearchBarInput: results[0].geometry.location.toJSON() }, () => {
+                this.filterData()
+            })
+        })
     }
-        
-      
-
-
+       
     componentDidMount() {
         this.getData()
         
