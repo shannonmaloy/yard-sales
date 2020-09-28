7ec5904921778130d125e7885f8a69a124c1e0d1
Shannon Maloy <shannon.maloy@gmail.com>
2020-09-28 02:47:11 -0400

    Completed dashboard view for a user

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,54 @@
+class DashboardController < ApplicationController
+    include CurrentUserConcern
+    def index
+        if @current_user
+            sales = Sale.where(user_id: @current_user)
+            
+            render json: {
+                sales: sales,
+                logged_in: true,
+                user: @current_user
+            }
+        else
+            render json: {
+                logged_in: false
+            }
+        end
+
+
+        
+    end
+
+    # def show
+    #     sale = Sale.find(params[:id])
+    #     render json: {sale: sale}
+    # end
+
+    def create
+        sale = Sale.create!(sale_params)
+        render json: { sale: sale }
+    end
+
+    def update
+        sale = Sale.find(params[:id])
+       sale.update(sale_params)
+       sale.save
+        render json: { sale: sale }
+        
+    end
+
+    def destroy
+        sale = Sale.find(params[:id])
+        sale.destroy
+        render json: {message: "Succesfully deleted"}
+    end
+
+    private
+    def sale_params
+        params.require(:sale).permit(:user_id, :address, :city, :state, :description, :zip, :date, :start_time, :end_time, :payment_type, :lat, :lng)
+    end
+
+ 
+    
+
+end
--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,11 +1,49 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
 
-const Dashboard = props => {
-    console.log("Hello from dashboard:" , props)
+const Dashboard = (props) => {
+    const [usersSales, setUsersSales] = useState([])
+    console.log("Hello from dashboard:", props)
+    useEffect(() => {
+        // fetch('/users')
+        //   .then(res => res.json())
+        //   .then(res => {
+        //     setData(res.users)
+        //   })
+        getData()
+      }, [])
+    
+
+    const getData = () => {
+        axios.get('http://localhost:3001/dashboard', { withCredentials: true })
+            .then(res => {
+                console.log(res.data.sales)
+                setUsersSales(res.data.sales)
+                    
+                
+            }).catch(err => {
+                console.log("check login", err)
+            }) 
+    }
+
+
     return (
         <div>
-            <div> {props.user.email} - Dashboard COMPONENT</div>
+            <div> Welcome back! {props.user.email}</div>
             <h1>Status: {props.loggedInStatus}</h1>
+            {console.log(usersSales)}
+            {usersSales ? (
+        usersSales.map((sale) => {
+          return (
+            <div className="sale" key={sale.id}>
+                  <h3>User ID:{sale.user_id} - {sale.address}, {sale.city}, {sale.state} {sale.zip}</h3>
+                  <p>{sale.id} - {sale.description}</p>
+              <p>Date & Time: {sale.date} </p>
+              <p>{sale.start_time} to {sale.end_time}</p>
+            </div>
+          );
+        })
+      ) : <p>No Yard Sales Posted...</p>}
         </div>
     )
 }
